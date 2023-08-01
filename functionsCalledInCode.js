bc5cca0b115399615b4af07cebeda3148edf3856
Luciano Brenner <133236080+Brenner-07@users.noreply.github.com>
2023-07-31 21:25:02 -0300

    Create functionsCalledInCode.js

---
+++ functionsCalledInCode.js
@@ -0,0 +1,36 @@
+function Create_Tabs(){
+
+var register = String
+var spreadsheet = SpreadsheetApp.getActive();
+    spreadsheet.getRange('B2').activate();
+}
+
+function Clear_CAP_Contents() {
+var spreadsheet = SpreadsheetApp.getActive();
+  spreadsheet.getRange('B2').activate();
+  spreadsheet.getActiveRangeList().clear({contentsOnly: true, skipFilteredRows: true});
+var spreadsheet = SpreadsheetApp.getActive();
+  spreadsheet.getRange('B3').activate();
+  spreadsheet.getActiveRangeList().clear({contentsOnly: true, skipFilteredRows: true});
+var spreadsheet = SpreadsheetApp.getActive();
+  spreadsheet.getRange('B4').activate();
+  spreadsheet.getActiveRangeList().clear({contentsOnly: true, skipFilteredRows: true});
+var spreadsheet = SpreadsheetApp.getActive();
+  spreadsheet.getRange('C6').activate();
+  spreadsheet.getActiveRangeList().clear({contentsOnly: true, skipFilteredRows: true});
+var spreadsheet = SpreadsheetApp.getActive();
+  spreadsheet.getRange('B8').activate();
+  spreadsheet.getActiveRangeList().clear({contentsOnly: true, skipFilteredRows: true});
+var spreadsheet = SpreadsheetApp.getActive();
+  spreadsheet.getRange('G8').activate();
+  spreadsheet.getActiveRangeList().clear({contentsOnly: true, skipFilteredRows: true});
+var spreadsheet = SpreadsheetApp.getActive();
+  spreadsheet.getRange('C9').activate();
+  spreadsheet.getActiveRangeList().clear({contentsOnly: true, skipFilteredRows: true});
+var spreadsheet = SpreadsheetApp.getActive();
+  spreadsheet.getRange('E9').activate();
+  spreadsheet.getActiveRangeList().clear({contentsOnly: true, skipFilteredRows: true});
+var spreadsheet = SpreadsheetApp.getActive();
+  spreadsheet.getRange('A19').activate();
+  spreadsheet.getActiveRangeList().clear({contentsOnly: true, skipFilteredRows: true});
+};
